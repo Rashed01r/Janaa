1a10cae5ba43be20a5c66959acacbab12c051e81
khlaed alwadai <117672053+khaled-alwadai2022@users.noreply.github.com>
2022-12-28 13:55:28 +0300

    b

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,25 @@
 import 'package:final_project/firebase_options.dart';
 import 'package:final_project/local/local.dart';
+import 'package:final_project/view/ActivityPage.dart';
+import 'package:final_project/view/Homehome.dart';
 import 'package:final_project/view/LoginPage.dart';
-
-
-
+import 'package:final_project/view/PlacesPage.dart';
+import 'package:final_project/view/ProfilePage.dart';
+import 'package:final_project/view/Singup.dart';
 import 'package:firebase_core/firebase_core.dart';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_storage/get_storage.dart';
 
-
+final box = GetStorage();
 
 void main() async {
-   await GetStorage.init();
-  
   WidgetsFlutterBinding.ensureInitialized();
+  await GetStorage.init();
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -33,7 +35,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       locale: Get.deviceLocale,
       translations: MyLocale(),
-      home:LoginPage(),
+      home: LoginPage(),
     );
   }
 }
